1a7cd183719232a22eb6c080cb2e64f377ffed29
zaid <khanzaid786786.kz@gmail.com>
2020-08-07 17:41:26 +0530

    added

--- src/Components/Booking/BookingBarChart/BookingBarChart.js
+++ src/Components/Booking/BookingBarChart/BookingBarChart.js
@@ -12,7 +12,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import "chartjs-top-round-bar";
+// import "chartjs-top-round-bar";
 import { useStyles } from "./css";
 
 export default function BarChart() {
